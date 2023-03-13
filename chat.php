df70bea86d7edf56d307278e049a8c232969d82d
Mohammad mahdi <work.mohamad.mahdi@gmail.com>
2023-03-13 12:56:09 +0330

    chat page has been made

--- chat.php
+++ chat.php
@@ -3,7 +3,7 @@ $title="Chat...";
 require_once "init.php";
 ?>
 <div class="col-11 col-lg-4 users-box row">
-        <a href="#" class="badge badge-secondary col-2 col-lg-1" style="height: 25px"><svg width="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></a>
+        <a href="index.php" class="badge badge-secondary col-2 col-lg-1" style="height: 25px"><svg width="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></a>
     <div class="user-box col-10 col-lg-11">
         <img src="assets/img/user-00.jpg">
         <div>
@@ -13,21 +13,45 @@ require_once "init.php";
             </p>
         </div>
     </div>
-
     <hr width="85%"/>
-    <div class="col-12">
-        <div class="chat outgoing">
-            <div>
-                <p>Hi buddy, How are you?</p>
-            </div>
+    <div class="col-12 chats">
+        <div class="outgoing">
+            This is jkjkad dsfn/sf sjflsdf sjfsfjlafs fsf sjdlf sdfsf djfmsg1
         </div>
-        <div class="chat incoming">
-            <div>
-                <p>Hi buddy, How are you too?</p>
-            </div>
+        <br>
+        <br>
+        <div class="incoming">
+            This is msg2hasjkdh d.jabdj kdad adajbdjada dajdband adjabdnad ajlda dndj
         </div>
-
-
+        <br>
+        <br>
+        <div class="incoming">
+            This is msg2hasjkdh d.jabdj kda adjabdnad ajlda dndj
+        </div>
+        <br>
+        <div class="outgoing">
+            ada akdja masdlk alkd
+        </div>
+        <br>
+        <br>
+        <div class="outgoing">
+            This is jkjkad dsfn/sf sjflsdf sjfsfjlafs fsf sjdlf sdfsf djfmsg1
+        </div>
+        <br>
+        <br>
+        <div class="incoming">
+            This is msg2hasjkdh d.jabdj kdad adajbdjada dajdband adjabdnad ajlda dndj
+        </div>
+        <br>
+        <br>
+        <div class="incoming">
+            This is msg2hasjkdh d.jabdj kda adjabdnad ajlda dndj
+        </div>
+        <br>
+        <div class="outgoing">
+            ada akdja masdlk alkd
+        </div>
+    </div>
 </div>
 <?php
 require_once "footer.php";
